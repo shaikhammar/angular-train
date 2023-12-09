d5036dc504d9b03df9e838b114b6313e12dabb06
Ammar Shaikh <shaikhammar87@gmail.com>
2023-12-09 06:43:52 +0000

    Updated forms and button controls to work

--- my-app/src/app/recipes/recipe.service.ts
+++ my-app/src/app/recipes/recipe.service.ts
@@ -1,14 +1,15 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  recipeSelected = new EventEmitter<Recipe>();
+  recipesChanged = new Subject<Recipe []>();
 
   private recipes: Recipe[] = [
     new Recipe('A Tasty Recipe', 
@@ -35,6 +36,21 @@ export class RecipeService {
 
   addIngredientToShoppingList(ingredients: Ingredient[]){
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number){
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 
 }
--- my-app/src/app/recipes/recipes.component.ts
+++ my-app/src/app/recipes/recipes.component.ts
@@ -1,21 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
-import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css',
-  providers: [RecipeService]
+  styleUrl: './recipes.component.css'
 })
 export class RecipesComponent implements OnInit {
   @Input() selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService){}
+  constructor(){}
 
   ngOnInit(){
-    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-      this.selectedRecipe = recipe;
-    })
+
   }
 }
